b30212867b635eeff20f8ded2ebf518c255f9e3e
4AmInLA <869840059@qq.com>
2021-07-19 21:30:31 +0800

    更新

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,30 +1,41 @@
 import React, { Component } from 'react'
+import PubSub from 'pubsub-js'
+import './index.css'
 
 export default class List extends Component {
+    //初始化状态
+    state={
+        users:[]
+    };
     render() {
         return (
             <div>
                 <div className="row">
-                    <div className="card">
-                            <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-                            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" alt={'atguigu'} style={{width: '100px'}}/>
-                            </a>
-                            <p className="card-text">reactjs</p>
-                        </div>
-                    <div className="card">
-                            <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-                            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" alt={'atguigu'} style={{width: '100px'}}/>
-                            </a>
-                            <p className="card-text">reactjs</p>
-                        </div>
-                    <div className="card">
-                            <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-                            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" alt={'atguigu'} style={{width: '100px'}}/>
-                            </a>
-                            <p className="card-text">reactjs</p>
-                        </div>
+                    {
+                        this.state.users.map((userObj)=>{
+                            return(
+                                <div className="card" key={userObj.login}>
+                                    <a href={userObj.html_url} target="_blank" rel="noreferrer">
+                                    <img src={userObj.avatar_url} alt={'atguigu'} style={{width: '100px'}}/>
+                                    </a>
+                                    <p className="card-text">{userObj.login}</p>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
         )
     }
+    componentDidMount(){
+        //订阅消息，用于接收search发布的数据PubSub.publish();
+        this.pid = PubSub.subscribe('Data',(_,data)=>{
+            console.log('接收到的数据',data);
+            //存起来初始化
+            this.setState({users:data});
+        });
+    };
+    componentWillUnmount(){
+        PubSub.unsubscribe(this.pid);
+    };
 }
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-
+import axios from 'axios';
+import PubSub from 'pubsub-js';
 export default class Search extends Component {
     render() {
         return (
@@ -7,10 +8,25 @@ export default class Search extends Component {
                 <section className="jumbotron">
                     <h3 className="jumbotron-heading">Search Github Users</h3>
                     <div>
-                        <input type="text" placeholder="enter the name you search"/>&nbsp;<button>Search</button>
+                        <input type="text" placeholder="enter the name you search" ref ={c=>this.keyword=c}/>&nbsp;
+                        <button onClick={this.search}>Search</button>
                     </div>
                 </section>
             </div>
         )
     }
+    search=()=>{
+        //获取用户输入的搜索框子value作为搜索关键字
+        const {value}= this.keyword;
+
+        //axios
+        // axios.get('https://api.github.com/search/users?q=${value})此处我用的辣鸡拼接（太low），优化为下↓
+        axios.get('https://api.github.com/search/users',{params:{q:value}}).then(
+            response => {
+                //console.log(response.data.items);
+                PubSub.publish('Data',response.data.items)
+            },error =>{
+                console.log(error);
+            })
+    };
 }
